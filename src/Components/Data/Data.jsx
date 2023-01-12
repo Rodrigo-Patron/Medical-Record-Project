3201eabaaf83ebb79e21ff63e851614ac991dece
Agatha A Msola <agatha@aims.ac.tz>
2023-01-12 01:08:49 +0100

    *Main Component -input form updated *Data Component -measurement units added

--- src/Components/Data/Data.jsx
+++ src/Components/Data/Data.jsx
@@ -3,12 +3,16 @@ import { useContext } from "react";
 import { MedicalContext } from "../../MedicalContext";
 
 const Data = () => {
-  const { bp } = useContext(MedicalContext);
+  const { bp,heartRate,sugar,oxygen,weight } = useContext(MedicalContext);
 
   return (
     <div className="Data">
       <h1>Here is the CHARTS and DATA</h1>
-      {bp} kg
+      <div> Blood Pressure: {bp} mmHg</div>
+      <div> Heart Rate: {heartRate} bpm</div>
+      <div> Sugar: {sugar} mM</div>
+      <div> Oxygen: {oxygen} LPM</div>
+      <div> Weight: {weight} Kg</div>
     </div>
   );
 };
