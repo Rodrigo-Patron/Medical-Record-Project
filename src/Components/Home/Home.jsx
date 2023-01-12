8272b7f5b42027b2ecf20408889b488a15843e8c
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-01-12 22:25:14 +0100

    add login functionality

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,37 +1,12 @@
-import { Button, Form } from "react-bootstrap";
 import "./Home.scss";
-import { NavLink } from "react-router-dom";
 import medicalPic from "../../img/pink-pic.jpg";
-import { useRef, useContext } from "react";
+import { useContext } from "react";
 import { MedicalContext } from "../../MedicalContext";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
 
 const Home = () => {
   const { setUser, user } = useContext(MedicalContext);
-
-  const inputName = useRef(null);
-  const inputEmail = useRef(null);
-  const inputPassword = useRef(null);
-
-  const submitRegisterForm = (e) => {
-    e.preventDefault();
-
-    console.log(inputName.current.value);
-    console.log(inputEmail.current.value);
-    console.log(inputPassword.current.value);
-
-    setUser([
-      {
-        name: inputName.current.value,
-        email: inputEmail.current.value,
-        password: inputPassword.current.value,
-      },
-      ...user
-    ]);
-  };
-
-  const submitLoginForm = (e) => {
-    e.preventDefault();
-  };
 
   return (
     <div className="Home">
@@ -39,54 +14,9 @@ const Home = () => {
       <div className="homeContainer">
         <img src={medicalPic} alt="" />
         <div className="formContainer">
-          {/* REGISTER FORM */}
+          <Register />
 
-          <Form onSubmit={submitRegisterForm}>
-            <h2>Register</h2>
-            <Form.Group className="formBox">
-              <Form.Label>Full name</Form.Label>
-              <Form.Control
-                type="text"
-                ref={inputName}
-                placeholder="Type name"
-                required
-              />
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                ref={inputEmail}
-                placeholder="Type email"
-                required
-              />
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={inputPassword}
-                placeholder="Type password"
-                required
-              />
-              <div className="Btn">
-                <Button type="submit">Register</Button>
-              </div>
-            </Form.Group>
-          </Form>
-
-          {/* LOGIN FORM */}
-
-          <Form onSubmit={submitLoginForm}>
-            <h2>Log in</h2>
-            <Form.Group className="formBox">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Type email" />
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Type password" />
-              <div className="Btn">
-                <NavLink to="main">
-                  <Button type="submit">Log in</Button>
-                </NavLink>
-              </div>
-            </Form.Group>
-          </Form>
+          <Login />
         </div>
       </div>
     </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,5 +1,69 @@
+import { Button, Form } from "react-bootstrap";
+import "./Register.scss";
+import { useRef, useContext } from "react";
+import { MedicalContext } from "../../MedicalContext";
+
 const Register = () => {
-  return <div></div>;
+  const { setUser, user } = useContext(MedicalContext);
+
+  console.log(user);
+  const inputName = useRef(null);
+  const inputEmail = useRef(null);
+  const inputPassword = useRef(null);
+
+  const submitRegisterForm = (e) => {
+    e.preventDefault();
+
+    // console.log(inputName.current.value);
+    // console.log(inputEmail.current.value);
+    // console.log(inputPassword.current.value);
+
+    setUser([
+      {
+        name: inputName.current.value,
+        email: inputEmail.current.value,
+        password: inputPassword.current.value,
+      },
+      ...user,
+    ]);
+    alert("You are now registered!");
+
+    inputName.current.value = "";
+    inputEmail.current.value = "";
+    inputPassword.current.value = "";
+  };
+
+  return (
+    <Form onSubmit={submitRegisterForm}>
+      <h2>Register</h2>
+      <Form.Group className="formBox">
+        <Form.Label>Full name</Form.Label>
+        <Form.Control
+          type="text"
+          ref={inputName}
+          placeholder="Type name"
+          required
+        />
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          ref={inputEmail}
+          placeholder="Type email"
+          required
+        />
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          ref={inputPassword}
+          placeholder="Type password"
+          required
+        />
+        <div className="Btn">
+          <Button type="submit">Register</Button>
+        </div>
+      </Form.Group>
+    </Form>
+  );
 };
 
 export default Register;
